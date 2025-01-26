ae20787531a38aed5e099adb8976b68243a78ef0
Mahab Rizwan <mahabrizwan@gmail.com>
2025-01-26 20:17:35 +0500

    update.tsx

--- src/app/Homepage4/page.tsx
+++ src/app/Homepage4/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import { FaStarHalf, FaStar } from "react-icons/fa";
 import Image from "next/image";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react';
 import Home from './Homepage1/page';
 import HomePage2 from './Homepage2/page';
